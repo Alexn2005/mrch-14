425d789ac20ab59e5a557f37894bc88545653704
Oleksandr Nyzhnyk <alexnyzhnyk2005@gmail.com>
2021-03-14 11:39:17 -0700

    added 3rd number

--- adding 2 numbers.py
+++ adding 2 numbers.py
@@ -1,9 +1,10 @@
 x = int(input('Insert a number you want to add: '))
 y = int(input('Insert another number you want to add: '))
+z = int(input('Insert another number you want to add: '))
 
 
-def adding_two_numbers(x, y):
-    return x + y
+def adding_two_numbers(x, y, z):
+    return x + y + z
 
 
-print(adding_two_numbers(x, y))
+print(adding_two_numbers(x, y, z))
